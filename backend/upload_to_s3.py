efeb3bb4435e111cd0f9e043e1ea57f10bf7b127
Angelina0905 <yudriangell@gmail>
2026-04-24 22:42:41 +0700

    ubah S3

--- backend/upload_to_s3.py
+++ backend/upload_to_s3.py
@@ -2,18 +2,17 @@ import boto3
 import os
 import uuid
 
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_REGION")
-)
-
 def upload_to_s3(file):
     try:
-        bucket_name = os.getenv("AWS_BUCKET_NAME")
+        s3 = boto3.client(
+            's3',
+            aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
+            aws_secret_access_key=os.getenv('AWS_SECRET_KEY'),
+            region_name=os.getenv('AWS_REGION')
+        )
 
-        # bikin nama file unik
+        bucket_name = os.getenv('AWS_BUCKET')
+
         filename = str(uuid.uuid4()) + "_" + file.filename
 
         s3.upload_fileobj(
@@ -21,15 +20,14 @@ def upload_to_s3(file):
             bucket_name,
             filename,
             ExtraArgs={
-                "ACL": "public-read",   # 🔥 biar bisa diakses link
+                "ACL": "public-read",
                 "ContentType": file.content_type
             }
         )
 
-        # URL file
         file_url = f"https://{bucket_name}.s3.amazonaws.com/{filename}"
         return file_url
 
     except Exception as e:
-        print("ERROR S3:", e)
+        print("S3 ERROR:", e)
         return None
